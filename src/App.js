942c28a7c9d66f345251e3e86d2988118ab5b056
mjohnstone14 <mjohnstone@eaglerockschool.org>
2020-04-20 07:04:51 -0700

    add material-ui package, add image to header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ResumeHeader from './components/ResumeHeader.js';
 import Education from './components/Education';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+
 function App() {
   return (
     <div className="App">
--- src/components/ResumeHeader.js
+++ src/components/ResumeHeader.js
@@ -1,12 +1,30 @@
 import React from 'react';
-const ResumeHeader = () => {
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    size: {
+      width: theme.spacing(30),
+      height: theme.spacing(30),
+      position: 'relative',
+      left: '2%',
+      top: '2%'
+    }
+  }));
+
+export default function ResumeHeader() {
+    const classes = useStyles();
     return (
         <header>
+            <Avatar alt="Marwan Johnstone" className={classes.size}/>
             Marwan Johnstone
         </header>
 
     )
-    
 }
-
-export default ResumeHeader;
