ac74bdb1dab105cb03436caa31aafe61fcc7f3ca
mjohnstone14 <mjohnstone@eaglerockschool.org>
2020-04-20 08:31:26 -0700

    begin styling header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import './App.css';
 import ResumeHeader from './components/ResumeHeader.js';
 import Education from './components/Education';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import './Styles/App.css';
 
 function App() {
+  const THEME = createMuiTheme({
+    typography: {
+      "fontFamily": "\"Roboto\",\"Helvetica\",\"Arial\",sans-serif",
+      "fontSize": 20,
+      "fontWeightLight": 300,
+      "fontWeightRegular": 400,
+      "fontWeightMedium": 500
+    }
+  })
   return (
-    <div className="App">
-         <ResumeHeader className="App-header"/>
-         <Education/>
-         <Experience/>
-         <Projects/>
-         <Skills/>
-    </div>
+    <MuiThemeProvider theme={THEME}>
+      <div className="App">
+        <ResumeHeader className="App-header"/>
+        <Education/>
+        <Experience/>
+        <Projects/>
+        <Skills/>
+      </div>
+    </MuiThemeProvider>
+    
   );
 }
 
--- src/components/ResumeHeader.js
+++ src/components/ResumeHeader.js
@@ -1,30 +1,62 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
+import photo from '../images/me.png';
+import { Grid } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     root: {
+      flexGrow: 1,
       display: 'flex',
       '& > *': {
         margin: theme.spacing(1),
       },
     },
-    size: {
+    avatar: {
       width: theme.spacing(30),
       height: theme.spacing(30),
-      position: 'relative',
-      left: '2%',
-      top: '2%'
+      borderStyle: 'solid',
+      borderColor: 'grey',
+      display: 'inline-block',
+      position: 'left'
+    },
+    title: {
+      fontSize: '80px',
+      borderStyle: 'solid',
+      borderRadius: '20px',
+      borderColor:  'lightgrey',
+      width: '90%'
     }
   }));
 
 export default function ResumeHeader() {
     const classes = useStyles();
     return (
-        <header>
-            <Avatar alt="Marwan Johnstone" className={classes.size}/>
-            Marwan Johnstone
-        </header>
-
+      <div className={classes.root}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Avatar alt="Marwan Johnstone" variant='rounded' className={classes.avatar} src={photo} />
+          </Grid>
+          <Grid item xs={6}>
+            <p className={classes.title}>Marwan Johnstone</p>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>xs=6</Paper>
+          </Grid> 
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=3</Paper>
+          </Grid>
+        </Grid>
+      </div>
     )
 }
