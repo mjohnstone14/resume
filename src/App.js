b54c080da2452602a87c9a71abea580af3718b64
mjohnstone14 <mjohnstone@eaglerockschool.org>
2020-04-21 11:42:54 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -4,29 +4,20 @@ import Education from './components/Education';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './styles/App.scss';
 
 function App() {
-  const THEME = createMuiTheme({
-    typography: {
-      "fontFamily": "\"Roboto\",\"Helvetica\",\"Arial\",sans-serif",
-      "fontSize": 20,
-      "fontWeightLight": 300,
-      "fontWeightRegular": 400,
-      "fontWeightMedium": 500
-    }
-  })
+
   return (
-    <MuiThemeProvider theme={THEME}>
-      <div className="App">
-        <ResumeHeader className="App-header"/>
-        <Education/>
-        <Experience/>
-        <Projects/>
-        <Skills/>
-      </div>
-    </MuiThemeProvider>
+  
+    <div className="App">
+      <ResumeHeader className="App-header"/>
+      <Education/>
+      <Experience/>
+      <Projects/>
+      <Skills/>
+    </div>
+
     
   );
 }
