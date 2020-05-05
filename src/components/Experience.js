b9c4c96c5d5cc66915e1766c8b30e14bdd7b7046
mjohnstone14 <mjohnstone@eaglerockschool.org>
2020-05-05 12:06:23 -0700

    update styles

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,6 +3,8 @@ import Accordion from './Accordion';
 import Card from './Card';
 import '../styles/experience.scss';
 import content from '../data/resumeContent'
+import down from '../images/down.svg'
+import up from '../images/up.svg'
 
 const Experience = () => {
     const [activeEventKey, setActiveEventKey] = useState(0);
@@ -12,9 +14,9 @@ const Experience = () => {
             {content.map(({ title, date, bullet, bullet2, bullet3, bullet4, bullet5, bullet6 }, index) => (
                 <Card key={index}>
                     <Accordion.Toggle element={Card.Header} eventKey={index}>
-                        <h4>{title}<span className="tabDate">{date}</span></h4>
-                        {activeEventKey !== index && <span>v</span>}
-                        {activeEventKey === index && <span>^</span>}
+                        <h4>{title}<br/><span className="tabDate">{date}</span></h4>
+                        {activeEventKey !== index && <img src={down} alt="down"/>}
+                        {activeEventKey === index && <img src={up} alt="up"/>}
                     </Accordion.Toggle>
                     <Accordion.Collapse eventKey={index} element={Card.Body}>
                         {bullet !== undefined ? <li>{bullet}</li> : null}
--- src/data/resumeContent.js
+++ src/data/resumeContent.js
@@ -1,6 +1,6 @@
 const resumeContent = [
     {
-        title: 'Nordstrom, Seattle WA',
+        title: 'Nordstrom, Seattle WA (Software Engineer, Full-time)',
         date: 'August 2019 - Present',
         bullet: 'Developed and continuously deployed an internal applications tool designed to process and inspect product returns in Nordstrom stores, inventory facilities, and fulfillment centers that would save ~$14.5M projected expenses for the company in 2020.',
         bullet2: 'Developed front-end facing applications designed to work on a company-wide scale using React Hooks, Sass, and Java Spring API',
@@ -10,21 +10,25 @@ const resumeContent = [
         bullet6:  'Created, managed, and resolved issues in GitLab and Jira boards to track progress with the team, as well as communicate goals with product and project management.'
     },
     {
-      title: 'Nordstrom, Seattle WA',
-      date: 'August 2019 - Present',
-      bullet: 'second contentsecond contensecond contensecond contensecond contensecond contensecond contensecond conten'
+      title: 'Nordstrom, Seattle WA (Full Stack Intern)',
+      date: 'May 2019 - August 2019',
+      bullet: 'Developed UI features and service endpoints to better improve the usability of Nordstrom’s internal clothing lookup tool.',
+      bullet2: 'Utilized frameworks such as React class components and a Python Flask service layer connected to DynamoDB in order to filter out relevant data for dropdown UI elements.',
+      bullet3: 'Designed and incorporated a dynamic search filter tool that could filter clothing items based on color, size, location, and item disposition.',
+      bullet4: 'Routinely used git to rebase and merge code, using source control to manage integration and production pipelines to continuously deliver updated and running code without interfering with the overarching project.'
   },  {
-      title: 'Nordstrom, Seattle WA',
-      date: 'August 2019 - Present',
-      bullet: 'second conten second contensecond contensecond contensecond contensecond contensecond contensecond contensecond contensecond contensecond contensecond content'
+      title: 'Nordstrom, Seattle WA (Software Engineer Intern)',
+      date: 'May 2018 - August 2018',
+      bullet: 'Developed an API proof of concept for Nordstrom’s supply chain utilizing Apache Kafka Streams.',
+      bullet2: 'Incorporated Java SpringBoot along with Docker and Kafka Streams to develop a service that would watch purchased orders over a time window, if those orders pertained to one customer then they would be consolidated instead of shipped in individual boxes.'
   },  {
-      title: 'Nordstrom, Seattle WA',
-      date: 'August 2019 - Present',
-      bullet: 'second content'
+      title: 'Studenterhuset, Copenhagen, Denmark (Bar Volunteer)',
+      date: 'August 2017 - May 2018',
+      bullet: 'Part of a volunteer crew of international students and Danes that would help the bar employees at Studenterhuset make drinks, maintain the bar, make sales and manage events.'
   },  {
-      title: 'Nordstrom, Seattle WA',
+      title: 'University of Puget Sound (Accommodations and Disability Tech Specialist)',
       date: 'August 2019 - Present',
-      bullet: 'second content'
+      bullet: 'Had multiple responsibilities that involved being a confidential student liaison for students with disabilities. Some of my tasks included speaking with disabled students and helping them with any technological needs or accommodations that they might have. One of my biggest responsibilities was coordinating a team of other workers to help scan physical copies of books into digital e-books. '
   }
   ];
 
