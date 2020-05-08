8eb45418831dfe369112206d611b2984086c36d6
mjohnstone14 <mjohnstone@eaglerockschool.org>
2020-05-08 13:10:45 -0700

    updated snap

--- src/components/ResumeHeader.js
+++ src/components/ResumeHeader.js
@@ -7,11 +7,11 @@ const ResumeHeader = () => {
     return (
       <Fragment>
         
-            <img alt="Marwan Johnstone" className="avatar" src={photo} />
+            <img rel="preload" alt="Marwan Johnstone" className="avatar" src={photo} />
             <h2 className="title">Marwan Johnstone</h2>
             <div className="iconPadding">
                 <a href="https://www.linkedin.com/in/mjohnstone14/"><img src={linkedIn} alt="linkedin" className="icon"/></a>
-                <a href="https://github.com/mjohnstone14"><img rel="preload" src={github} alt="github" className="icon"/></a>
+                <a href="https://github.com/mjohnstone14"><img src={github} alt="github" className="icon"/></a>
             </div>     
       </Fragment>
       
