30a2d967d2263f60f5a1a679d102bc37cae5bcf2
mjohnstone14 <mjohnstone@eaglerockschool.org>
2020-05-08 13:03:38 -0700

    preload

--- src/components/ResumeHeader.js
+++ src/components/ResumeHeader.js
@@ -11,7 +11,7 @@ const ResumeHeader = () => {
             <h2 className="title">Marwan Johnstone</h2>
             <div className="iconPadding">
                 <a href="https://www.linkedin.com/in/mjohnstone14/"><img src={linkedIn} alt="linkedin" className="icon"/></a>
-                <a href="https://github.com/mjohnstone14"><img src={github} alt="github" className="icon"/></a>
+                <a href="https://github.com/mjohnstone14"><img rel="preload" src={github} alt="github" className="icon"/></a>
             </div>     
       </Fragment>
       
